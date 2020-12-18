ff5d2b136d77463b156009253a021949930af5b7
Xutrurn <kanjoskillz@gmail.com>
2020-12-18 10:43:24 +0100

    01 somma con 5 prompt, if loop e while

---
+++ 01/js/script.js
@@ -0,0 +1,36 @@
+// alert("Hello world");
+
+
+// SOMMA DI NUMERI CON 5 PROMPT FOR LOOP
+var somma = 0;
+for (var i = 0; i < 5; i++) {
+  var numero = parseInt(prompt("inserisci numero"));
+  somma += numero;
+}
+console.log(somma);
+
+
+
+// SOMMA DI NUMERI CON 5 PROMPT WHILE
+var somma = 0;
+var i = 0;
+while (i < 5) {
+  var numero = parseInt(prompt("inserisci numero"));
+  somma += numero;
+  i++;
+}
+console.log(somma);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log();
