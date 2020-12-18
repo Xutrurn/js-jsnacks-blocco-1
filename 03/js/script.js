758b11d7a1bbf880141644eb19735d418b17f22a
Xutrurn <kanjoskillz@gmail.com>
2020-12-18 12:23:21 +0100

    Prompt invito festa con for loop

--- 03/js/script.js
+++ 03/js/script.js
@@ -6,19 +6,42 @@
 
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
+// 1 Soluzione
+
+// var invitati = ["Nick", "Daisy", "James", "Tom"];
+//
+// var utente = prompt("Inserisci nome");
+// utente = utente.charAt(0).toUpperCase() + utente.substr(1).toLowerCase();
+// console.log(utente);
+//
+// if (invitati.includes(utente)) {
+//   console.log("sei stato invitato");
+//
+// } else {
+//   console.log("non sei stato invitato");
+// }
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+// 2 Soluzione con For Loop
 
 var invitati = ["Nick", "Daisy", "James", "Tom"];
-
 var utente = prompt("Inserisci nome");
 utente = utente.charAt(0).toUpperCase() + utente.substr(1).toLowerCase();
-console.log(utente);
 
-if (invitati.includes(utente)) {
-  console.log("sei stato invitato");
 
-} else {
-  console.log("non sei stato invitato");
+for (var i = 0; i < invitati.length; i++) {
+  if (utente == invitati[i]) {
+    console.log("sei stato invitato");
+  } else {
+    console.log("non sei stato invitato");
+
+  }
+
 }
+
+
+
 
 
 
