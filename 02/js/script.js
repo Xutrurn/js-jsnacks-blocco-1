105b6f371afede017b54cd268ffc39d1e200ddac
Xutrurn <kanjoskillz@gmail.com>
2020-12-18 11:03:12 +0100

    02 Prompt 6 volte numero, se dispari inserirlo in array vuoto...

---
+++ 02/js/script.js
@@ -0,0 +1,23 @@
+// alert("Hello world");
+
+
+// Prompt 6 volte numero, se dispari inserirlo in array vuoto...
+var arr = [];
+for (var i = 0; i < 6; i++) {
+  var numero = parseInt(prompt("inserisci numero"));
+  if (numero % 2 != 0) {
+    arr.push(numero);
+    console.log(arr);
+  }
+
+}
+
+
+
+
+
+
+
+
+
+// console.log();
